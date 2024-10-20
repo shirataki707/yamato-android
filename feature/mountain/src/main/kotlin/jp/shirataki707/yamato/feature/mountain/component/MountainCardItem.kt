40b00b96a2457eece4045de0d40318ecb195951d
shogo.takasaki <stakasaki707@gmail.com>
2024-10-20 23:57:09 +0900

    Add animation for mountain description

--- feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/component/MountainCardItem.kt
+++ feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/component/MountainCardItem.kt
@@ -1,13 +1,31 @@
 package jp.shirataki707.yamato.feature.mountain.component
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -30,18 +48,53 @@ fun MountainCardItem(
         .getDrawableResIdFromFileName(context, mountain.imageName)
         ?: R.drawable.place_holder
 
+    var expanded by remember { mutableStateOf(false) }
+    val color by animateColorAsState(
+        targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
+        else MaterialTheme.colorScheme.primaryContainer
+    )
+
     Card(modifier = modifier) {
-        Column {
+        Column(
+            modifier = Modifier
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioLowBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
+                .background(color = color)
+        ) {
             Image(
                 painter = painterResource(id = mountainImageResId),
                 contentDescription = mountain.name,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier.fillMaxWidth().height(200.dp),
             )
-            Text(
-                text = mountain.name,
-                fontSize = 20.sp
-            )
+            Column(modifier = Modifier.padding(8.dp)) {
+                Row {
+                    Text(
+                        text = mountain.name,
+                        fontSize = 24.sp
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(
+                        onClick = { expanded = !expanded },
+                    ) {
+                        Icon(
+                            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.secondary
+                        )
+                    }
+                }
+                Text(
+                    text = "標高: ${mountain.elevation}m",
+                )
+                Text(
+                    text = if (expanded) mountain.longDescription else mountain.shortDescription,
+                )
+            }
         }
     }
 }
