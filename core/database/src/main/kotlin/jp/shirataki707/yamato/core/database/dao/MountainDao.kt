864b6499ff0aeefcff03534557afc1825924f7c6
shogo.takasaki <stakasaki707@gmail.com>
2024-10-20 14:35:11 +0900

    Add MountainEntity

---
+++ core/database/src/main/kotlin/jp/shirataki707/yamato/core/database/dao/MountainDao.kt
@@ -0,0 +1,4 @@
+package jp.shirataki707.yamato.core.database.dao
+
+interface MountainDao {
+}
---
+++ core/database/src/main/kotlin/jp/shirataki707/yamato/core/database/model/MountainEntity.kt
@@ -0,0 +1,20 @@
+package jp.shirataki707.yamato.core.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "mountains")
+data class MountainEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val imageName: String,
+    val shortDescription: String,
+    val longDescription: String,
+    val elevation: Int,
+    val latitude: Double,
+    val longitude: Double,
+    @ColumnInfo(defaultValue = "false")
+    val isClimbed: Boolean,
+)
