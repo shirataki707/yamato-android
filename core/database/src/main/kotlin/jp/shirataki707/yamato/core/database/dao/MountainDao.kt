1ad6fba8860cdbac03f1a4f6d00cca0aa3582529
shogo.takasaki <stakasaki707@gmail.com>
2024-10-20 14:53:48 +0900

    Add MountainDao

--- core/database/src/main/kotlin/jp/shirataki707/yamato/core/database/dao/MountainDao.kt
+++ core/database/src/main/kotlin/jp/shirataki707/yamato/core/database/dao/MountainDao.kt
@@ -1,4 +1,23 @@
 package jp.shirataki707.yamato.core.database.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import jp.shirataki707.yamato.core.database.model.MountainEntity
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * DAO for [MountainEntity] access
+ */
+@Dao
 interface MountainDao {
+    @Query(
+        value = """
+        SELECT * FROM mountains
+        WHERE id = :mountainId
+    """,
+    )
+    fun getMountainEntity(mountainId: String): Flow<MountainEntity>
+
+    @Query("SELECT * FROM mountains")
+    fun getMountainEntities(): Flow<List<MountainEntity>>
 }
