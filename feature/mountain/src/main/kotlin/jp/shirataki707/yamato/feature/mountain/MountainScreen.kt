cf51d31c7ca78d469f2b5ead0bb1193f70d394bd
shogo.takasaki <stakasaki707@gmail.com>
2024-10-20 23:18:18 +0900

    Add instant MountainCardSection using lazyColumn

--- feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/MountainScreen.kt
+++ feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/MountainScreen.kt
@@ -4,14 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.shirataki707.yamato.core.database.model.MountainEntity
+import jp.shirataki707.yamato.feature.mountain.section.MountainCardSection
 
 @Composable
 fun MountainScreen(
-    viewModel: MountainViewModel = viewModel(),
     modifier: Modifier = Modifier,
+    viewModel: MountainViewModel = viewModel(),
 ) {
+    val mountains = viewModel.mountains.collectAsState()
 
+    MountainCardSection(
+        mountains = mountains.value,
+        modifier = modifier,
+    )
 }
--- feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/MountainViewModel.kt
+++ feature/mountain/src/main/kotlin/jp/shirataki707/yamato/feature/mountain/MountainViewModel.kt
@@ -6,6 +6,9 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.shirataki707.yamato.core.data.repository.MountainRepository
 import jp.shirataki707.yamato.core.database.model.MountainEntity
+import jp.shirataki707.yamato.core.model.data.Mountain
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -14,6 +17,9 @@ class MountainViewModel @Inject constructor(
     private val mountainRepository: MountainRepository
 ) : ViewModel() {
 
+    private val _mountains = MutableStateFlow<List<Mountain>>(emptyList())
+    val mountains: StateFlow<List<Mountain>> = _mountains
+
     init {
         fetchData()
     }
@@ -21,6 +27,7 @@ class MountainViewModel @Inject constructor(
     private fun fetchData() {
         viewModelScope.launch {
             mountainRepository.getMountains().collect { data ->
+                _mountains.value = data
                 Log.d("MountainViewModel", "Fetched data: $data")
             }
         }
