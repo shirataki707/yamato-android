3efa138f087a1e5ab6eb0ae86ba5a920017c8020
shogo.takasaki <stakasaki707@gmail.com>
2024-11-02 17:48:47 +0900

    Add feature/home module

---
+++ feature/home/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.yamato.android.feature)
+    alias(libs.plugins.yamato.android.library.compose)
+}
+
+android {
+    namespace = "jp.shirataki707.feature.home"
+}
+
+dependencies {
+
+}
