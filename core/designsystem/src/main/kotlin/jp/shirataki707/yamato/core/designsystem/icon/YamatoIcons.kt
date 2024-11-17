25cd61bbd8c2adc983bcff95f6bc2b1fd1e2cc6b
shogo.takasaki <stakasaki707@gmail.com>
2024-11-17 18:01:14 +0900

    Add button and background to design system

--- core/designsystem/src/main/kotlin/jp/shirataki707/yamato/core/designsystem/icon/YamatoIcons.kt
+++ core/designsystem/src/main/kotlin/jp/shirataki707/yamato/core/designsystem/icon/YamatoIcons.kt
@@ -3,8 +3,10 @@ package jp.shirataki707.yamato.core.designsystem.icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.rounded.Add
 
 object YamatoIcons {
+    val Add = Icons.Rounded.Add
     val ExpandLess = Icons.Filled.ExpandLess
     val ExpandMore = Icons.Filled.ExpandMore
 }
