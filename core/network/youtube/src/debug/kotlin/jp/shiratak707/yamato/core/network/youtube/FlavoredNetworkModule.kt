0b0b64f6861b1a35fb7008373562dbc50c2d1d65
shogo.takasaki <stakasaki707@gmail.com>
2024-11-21 22:38:07 +0900

    Add common error content in :core:ui

--- core/network/youtube/src/debug/kotlin/jp/shiratak707/yamato/core/network/youtube/FlavoredNetworkModule.kt
+++ core/network/youtube/src/debug/kotlin/jp/shiratak707/yamato/core/network/youtube/FlavoredNetworkModule.kt
@@ -6,11 +6,13 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import jp.shirataki707.yamato.core.network.youtube.YoutubeDataSource
 import jp.shirataki707.yamato.core.network.youtube.demo.DemoYoutubeDataSource
+import jp.shirataki707.yamato.core.network.youtube.retorofit.YoutubeApiClient
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal interface FlavoredNetworkModule {
 
     @Binds
-    fun binds(impl: DemoYoutubeDataSource): YoutubeDataSource
+//    fun binds(impl: DemoYoutubeDataSource): YoutubeDataSource
+    fun binds(impl: YoutubeApiClient): YoutubeDataSource
 }
--- feature/home/src/main/kotlin/jp/shirataki707/yamato/feature/home/section/HomeInitialSection.kt
+++ feature/home/src/main/kotlin/jp/shirataki707/yamato/feature/home/section/HomeInitialSection.kt
@@ -1,7 +1,12 @@
 package jp.shirataki707.yamato.feature.home.section
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import jp.shirataki707.yamato.core.ui.common.CommonErrorContent
+import jp.shirataki707.yamato.core.ui.common.ParcelableResult
 
 @Composable
 internal fun HomeInitialSection(
@@ -9,4 +14,12 @@ internal fun HomeInitialSection(
     paddingValues: PaddingValues,
 ) {
     // TODO: Implement the initial section include blank and error
+    if (sectionState.videoResources is ParcelableResult.Failure) {
+        CommonErrorContent(
+            onReloadButtonClick = sectionState.initialLoadIfNeeded,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        )
+    }
 }
