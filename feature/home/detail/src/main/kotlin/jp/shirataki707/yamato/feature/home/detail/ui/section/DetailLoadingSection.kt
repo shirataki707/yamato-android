90c982e399d7088c65f7dfeb66b6e5f2021128f5
shogo.takasaki <stakasaki707@gmail.com>
2024-12-01 11:23:48 +0900

    Add loading section for detail

--- feature/home/detail/src/main/kotlin/jp/shirataki707/yamato/feature/home/detail/ui/section/DetailLoadingSection.kt
+++ feature/home/detail/src/main/kotlin/jp/shirataki707/yamato/feature/home/detail/ui/section/DetailLoadingSection.kt
@@ -1,2 +1,24 @@
 package jp.shirataki707.yamato.feature.home.detail.ui.section
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+internal fun DetailLoadingSection(
+    sectionState: DetailLoadingSectionState,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary,
+        )
+    }
+}
