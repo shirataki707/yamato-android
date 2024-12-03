12fec08e2aa4b72a6166f1b00cd7dbe95e394b9c
shogo.takasaki <stakasaki707@gmail.com>
2024-12-03 23:29:17 +0900

    Add unit test for VideoResourceRepository

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.yamato.android.library)
     alias(libs.plugins.yamato.hilt)
+    alias(libs.plugins.junit5)
 }
 
 android {
@@ -10,4 +11,11 @@ android {
 dependencies {
     api(projects.core.network.youtube)
     api(projects.core.database)
+
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.engine)
+    testImplementation(libs.junit.jupiter.params)
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
 }
